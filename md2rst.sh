84ac5dae3c34ba51c1027ed315969ec6a8043ab7
Juan <juancarlospaco@gmail.com>
2015-03-01 23:21:17 -0300

    [SKIP CI] update readme

--- md2rst.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-#    sudo apt-get install pandoc
-
-chrt --verbose --idle 0 pandoc --from=markdown_github --to=rst --smart --output=README.rst README.md
